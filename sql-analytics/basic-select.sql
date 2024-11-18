944b8e3be9a500434dbe2ed75ac574a96fedc025
ultimadux <66224770+ultimadux@users.noreply.github.com>
2024-11-17 23:27:04 -0500

    Update basic-select.sql

--- sql-analytics/basic-select.sql
+++ sql-analytics/basic-select.sql
@@ -1,8 +1,8 @@
 -- refer to the database created in the database-admin folder
 
-SELECT * FROM employees e
-ORDER BY salary ASC;
+SELECT * FROM employees e -- show all columns from employees
+ORDER BY salary ASC; -- order the table from lowest to highest vertically
 
-SELECT *
-FROM departments
+SELECT * -- show all columns from the table specified
+FROM departments -- (table specified)
 ORDER BY dept_id ASC;
