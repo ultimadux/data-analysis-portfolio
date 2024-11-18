1c618c404b858d2af69858f6c6955b8c50464274
ultimadux <66224770+ultimadux@users.noreply.github.com>
2024-11-17 23:35:14 -0500

    Update readme.sql

--- excel-analytics/readme.sql
+++ excel-analytics/readme.sql
@@ -1,2 +1,2 @@
--- the data used within this folder is exported from the database in database-admin. refer to data-base admin for original source code.
--- the only exception is lookups-exercises, which uses a dataset made (painfully) by hand.
+/* the data used within this folder is exported from the database in database-admin. refer to data-base admin for original source code.
+   the only exception is lookups-exercises, which uses a dataset made (painfully) by hand. */
