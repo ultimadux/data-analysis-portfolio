940f5042956a820aa611dab3dd5b25679484e957
ultimadux <66224770+ultimadux@users.noreply.github.com>
2024-11-18 23:17:38 -0500

    Update basic-select.sql

--- sql-analytics/basic-select.sql
+++ sql-analytics/basic-select.sql
@@ -1,8 +1,17 @@
 -- refer to the database created in the database-admin folder
 
 SELECT * FROM employees e -- show all columns from employees
+
+-- in the event that I want to order the values returned, I should use ORDER BY.
+
+SELECT * FROM employees e
 ORDER BY salary ASC; -- order the table from lowest to highest vertically
 
-SELECT * -- show all columns from the table specified
-FROM departments -- (table specified)
+-- it is also important to note that multiple queries can be called at once (at least in SSMS and Azure)
+
+SELECT * FROM employees e
+ORDER BY salary ASC;
+
+SELECT *
+FROM departments
 ORDER BY dept_id ASC;
