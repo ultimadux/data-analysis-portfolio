b52af3c5d052b39e13fa25faebf69e1db58b856b
ultimadux <66224770+ultimadux@users.noreply.github.com>
2024-11-19 02:18:28 -0500

    Update readme.sql

--- database-admin/readme.sql
+++ database-admin/readme.sql
@@ -1,1 +1,1 @@
--- this is where the core database is located. all other folders use or reference this database. 
+/* this is where the core database is located. all other folders use or reference this database. */
