a8fe49339dec41da9d00ea96b5e7a95e9c007225
ultimadux <66224770+ultimadux@users.noreply.github.com>
2024-11-23 23:37:21 -0500

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,5 @@
+"""
+  welcome to my data engineer profile!
+  this repository is updated 2-3 times weekly and is filled with both techniques and mini-projects. 
+  feel free to look around, and don't be afraid to bookmark the repository for later!
+"""
